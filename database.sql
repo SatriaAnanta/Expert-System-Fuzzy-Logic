f76e9b9dd64050931ecb650918ccccd8fb3f9574
SatriaAnanta <satriaanantagaming@gmail.com>
2020-12-21 20:21:09 +0800

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,446 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Dec 21, 2020 at 12:20 PM
+-- Server version: 5.7.25
+-- PHP Version: 7.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `dbfuzzy`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auth_group`
+--
+
+CREATE TABLE `auth_group` (
+  `id` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auth_group_permissions`
+--
+
+CREATE TABLE `auth_group_permissions` (
+  `id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auth_permission`
+--
+
+CREATE TABLE `auth_permission` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `content_type_id` int(11) NOT NULL,
+  `codename` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `auth_permission`
+--
+
+INSERT INTO `auth_permission` (`id`, `name`, `content_type_id`, `codename`) VALUES
+(1, 'Can add cuaca', 1, 'add_cuaca'),
+(2, 'Can change cuaca', 1, 'change_cuaca'),
+(3, 'Can delete cuaca', 1, 'delete_cuaca'),
+(4, 'Can view cuaca', 1, 'view_cuaca'),
+(5, 'Can add log entry', 2, 'add_logentry'),
+(6, 'Can change log entry', 2, 'change_logentry'),
+(7, 'Can delete log entry', 2, 'delete_logentry'),
+(8, 'Can view log entry', 2, 'view_logentry'),
+(9, 'Can add permission', 3, 'add_permission'),
+(10, 'Can change permission', 3, 'change_permission'),
+(11, 'Can delete permission', 3, 'delete_permission'),
+(12, 'Can view permission', 3, 'view_permission'),
+(13, 'Can add group', 4, 'add_group'),
+(14, 'Can change group', 4, 'change_group'),
+(15, 'Can delete group', 4, 'delete_group'),
+(16, 'Can view group', 4, 'view_group'),
+(17, 'Can add user', 5, 'add_user'),
+(18, 'Can change user', 5, 'change_user'),
+(19, 'Can delete user', 5, 'delete_user'),
+(20, 'Can view user', 5, 'view_user'),
+(21, 'Can add content type', 6, 'add_contenttype'),
+(22, 'Can change content type', 6, 'change_contenttype'),
+(23, 'Can delete content type', 6, 'delete_contenttype'),
+(24, 'Can view content type', 6, 'view_contenttype'),
+(25, 'Can add session', 7, 'add_session'),
+(26, 'Can change session', 7, 'change_session'),
+(27, 'Can delete session', 7, 'delete_session'),
+(28, 'Can view session', 7, 'view_session');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auth_user`
+--
+
+CREATE TABLE `auth_user` (
+  `id` int(11) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `last_login` datetime(6) DEFAULT NULL,
+  `is_superuser` tinyint(1) NOT NULL,
+  `username` varchar(150) NOT NULL,
+  `first_name` varchar(150) NOT NULL,
+  `last_name` varchar(150) NOT NULL,
+  `email` varchar(254) NOT NULL,
+  `is_staff` tinyint(1) NOT NULL,
+  `is_active` tinyint(1) NOT NULL,
+  `date_joined` datetime(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auth_user_groups`
+--
+
+CREATE TABLE `auth_user_groups` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `auth_user_user_permissions`
+--
+
+CREATE TABLE `auth_user_user_permissions` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `django_admin_log`
+--
+
+CREATE TABLE `django_admin_log` (
+  `id` int(11) NOT NULL,
+  `action_time` datetime(6) NOT NULL,
+  `object_id` longtext,
+  `object_repr` varchar(200) NOT NULL,
+  `action_flag` smallint(5) UNSIGNED NOT NULL,
+  `change_message` longtext NOT NULL,
+  `content_type_id` int(11) DEFAULT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `django_content_type`
+--
+
+CREATE TABLE `django_content_type` (
+  `id` int(11) NOT NULL,
+  `app_label` varchar(100) NOT NULL,
+  `model` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `django_content_type`
+--
+
+INSERT INTO `django_content_type` (`id`, `app_label`, `model`) VALUES
+(2, 'admin', 'logentry'),
+(4, 'auth', 'group'),
+(3, 'auth', 'permission'),
+(5, 'auth', 'user'),
+(6, 'contenttypes', 'contenttype'),
+(1, 'prakiraan', 'cuaca'),
+(7, 'sessions', 'session');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `django_migrations`
+--
+
+CREATE TABLE `django_migrations` (
+  `id` int(11) NOT NULL,
+  `app` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `applied` datetime(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `django_migrations`
+--
+
+INSERT INTO `django_migrations` (`id`, `app`, `name`, `applied`) VALUES
+(1, 'contenttypes', '0001_initial', '2020-12-17 05:38:00.116114'),
+(2, 'auth', '0001_initial', '2020-12-17 05:38:00.279760'),
+(3, 'admin', '0001_initial', '2020-12-17 05:38:00.564647'),
+(4, 'admin', '0002_logentry_remove_auto_add', '2020-12-17 05:38:00.625377'),
+(5, 'admin', '0003_logentry_add_action_flag_choices', '2020-12-17 05:38:00.636338'),
+(6, 'contenttypes', '0002_remove_content_type_name', '2020-12-17 05:38:00.700561'),
+(7, 'auth', '0002_alter_permission_name_max_length', '2020-12-17 05:38:00.734874'),
+(8, 'auth', '0003_alter_user_email_max_length', '2020-12-17 05:38:00.770046'),
+(9, 'auth', '0004_alter_user_username_opts', '2020-12-17 05:38:00.781472'),
+(10, 'auth', '0005_alter_user_last_login_null', '2020-12-17 05:38:00.811770'),
+(11, 'auth', '0006_require_contenttypes_0002', '2020-12-17 05:38:00.815194'),
+(12, 'auth', '0007_alter_validators_add_error_messages', '2020-12-17 05:38:00.827670'),
+(13, 'auth', '0008_alter_user_username_max_length', '2020-12-17 05:38:00.861195'),
+(14, 'auth', '0009_alter_user_last_name_max_length', '2020-12-17 05:38:00.892445'),
+(15, 'auth', '0010_alter_group_name_max_length', '2020-12-17 05:38:00.923721'),
+(16, 'auth', '0011_update_proxy_permissions', '2020-12-17 05:38:00.935691'),
+(17, 'auth', '0012_alter_user_first_name_max_length', '2020-12-17 05:38:00.968026'),
+(18, 'prakiraan', '0001_initial', '2020-12-17 05:38:00.994506'),
+(19, 'sessions', '0001_initial', '2020-12-17 05:38:01.018770');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `django_session`
+--
+
+CREATE TABLE `django_session` (
+  `session_key` varchar(40) NOT NULL,
+  `session_data` longtext NOT NULL,
+  `expire_date` datetime(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prakiraan_cuaca`
+--
+
+CREATE TABLE `prakiraan_cuaca` (
+  `id` int(11) NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `sweat` double NOT NULL,
+  `cape` double NOT NULL,
+  `rh700` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `prakiraan_cuaca`
+--
+
+INSERT INTO `prakiraan_cuaca` (`id`, `title`, `sweat`, `cape`, `rh700`) VALUES
+(1, 'Data Test 1', 241, 30.42, 82),
+(4, 'Data Test 2', 206.19, 3.81, 62),
+(5, 'Data Test 3', 205.39, 103.36, 58),
+(6, 'Data Test 4', 237.29, 25.19, 66),
+(7, 'Data Test 5', 212.41, 319.92, 91),
+(8, 'Data Test 6', 70.61, 1.07, 54),
+(9, 'Data Test 7', 97.81, 28.67, 40),
+(10, 'Data Test 8', 203.6, 5.3, 46),
+(11, 'Data Test 9', 204.81, 625.36, 79),
+(12, 'Data Test 10', 212.39, 145.23, 79),
+(13, 'Data Test 11', 214.81, 323.35, 88),
+(14, 'Data Test 12', 217.21, 1342.52, 79),
+(15, 'Data Test 13', 193.61, 182.72, 79),
+(16, 'Data Test 14', 231.53, 1572, 96),
+(17, 'Data Test 15', 232.4, 629.55, 90),
+(18, 'Data Test 16', 238, 169.26, 81),
+(19, 'Data Test 17', 218.21, 195.16, 80),
+(20, 'Data Test 18', 211.41, 302.98, 71),
+(21, 'Data Test 19', 191.79, 61.14, 62),
+(22, 'Data Test 20', 219.8, 85.56, 87);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `auth_group`
+--
+ALTER TABLE `auth_group`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `auth_group_permissions`
+--
+ALTER TABLE `auth_group_permissions`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `auth_group_permissions_group_id_permission_id_0cd325b0_uniq` (`group_id`,`permission_id`),
+  ADD KEY `auth_group_permissio_permission_id_84c5c92e_fk_auth_perm` (`permission_id`);
+
+--
+-- Indexes for table `auth_permission`
+--
+ALTER TABLE `auth_permission`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `auth_permission_content_type_id_codename_01ab375a_uniq` (`content_type_id`,`codename`);
+
+--
+-- Indexes for table `auth_user`
+--
+ALTER TABLE `auth_user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `auth_user_groups`
+--
+ALTER TABLE `auth_user_groups`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `auth_user_groups_user_id_group_id_94350c0c_uniq` (`user_id`,`group_id`),
+  ADD KEY `auth_user_groups_group_id_97559544_fk_auth_group_id` (`group_id`);
+
+--
+-- Indexes for table `auth_user_user_permissions`
+--
+ALTER TABLE `auth_user_user_permissions`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `auth_user_user_permissions_user_id_permission_id_14a6b632_uniq` (`user_id`,`permission_id`),
+  ADD KEY `auth_user_user_permi_permission_id_1fbb5f2c_fk_auth_perm` (`permission_id`);
+
+--
+-- Indexes for table `django_admin_log`
+--
+ALTER TABLE `django_admin_log`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `django_admin_log_content_type_id_c4bce8eb_fk_django_co` (`content_type_id`),
+  ADD KEY `django_admin_log_user_id_c564eba6_fk_auth_user_id` (`user_id`);
+
+--
+-- Indexes for table `django_content_type`
+--
+ALTER TABLE `django_content_type`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `django_content_type_app_label_model_76bd3d3b_uniq` (`app_label`,`model`);
+
+--
+-- Indexes for table `django_migrations`
+--
+ALTER TABLE `django_migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `django_session`
+--
+ALTER TABLE `django_session`
+  ADD PRIMARY KEY (`session_key`),
+  ADD KEY `django_session_expire_date_a5c62663` (`expire_date`);
+
+--
+-- Indexes for table `prakiraan_cuaca`
+--
+ALTER TABLE `prakiraan_cuaca`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `auth_group`
+--
+ALTER TABLE `auth_group`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `auth_group_permissions`
+--
+ALTER TABLE `auth_group_permissions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `auth_permission`
+--
+ALTER TABLE `auth_permission`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+--
+-- AUTO_INCREMENT for table `auth_user`
+--
+ALTER TABLE `auth_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `auth_user_groups`
+--
+ALTER TABLE `auth_user_groups`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `auth_user_user_permissions`
+--
+ALTER TABLE `auth_user_user_permissions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `django_admin_log`
+--
+ALTER TABLE `django_admin_log`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `django_content_type`
+--
+ALTER TABLE `django_content_type`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `django_migrations`
+--
+ALTER TABLE `django_migrations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `prakiraan_cuaca`
+--
+ALTER TABLE `prakiraan_cuaca`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `auth_group_permissions`
+--
+ALTER TABLE `auth_group_permissions`
+  ADD CONSTRAINT `auth_group_permissio_permission_id_84c5c92e_fk_auth_perm` FOREIGN KEY (`permission_id`) REFERENCES `auth_permission` (`id`),
+  ADD CONSTRAINT `auth_group_permissions_group_id_b120cbf9_fk_auth_group_id` FOREIGN KEY (`group_id`) REFERENCES `auth_group` (`id`);
+
+--
+-- Constraints for table `auth_permission`
+--
+ALTER TABLE `auth_permission`
+  ADD CONSTRAINT `auth_permission_content_type_id_2f476e4b_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`);
+
+--
+-- Constraints for table `auth_user_groups`
+--
+ALTER TABLE `auth_user_groups`
+  ADD CONSTRAINT `auth_user_groups_group_id_97559544_fk_auth_group_id` FOREIGN KEY (`group_id`) REFERENCES `auth_group` (`id`),
+  ADD CONSTRAINT `auth_user_groups_user_id_6a12ed8b_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`);
+
+--
+-- Constraints for table `auth_user_user_permissions`
+--
+ALTER TABLE `auth_user_user_permissions`
+  ADD CONSTRAINT `auth_user_user_permi_permission_id_1fbb5f2c_fk_auth_perm` FOREIGN KEY (`permission_id`) REFERENCES `auth_permission` (`id`),
+  ADD CONSTRAINT `auth_user_user_permissions_user_id_a95ead1b_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`);
+
+--
+-- Constraints for table `django_admin_log`
+--
+ALTER TABLE `django_admin_log`
+  ADD CONSTRAINT `django_admin_log_content_type_id_c4bce8eb_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`),
+  ADD CONSTRAINT `django_admin_log_user_id_c564eba6_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`);
